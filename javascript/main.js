1fd4e479940ac52a84b5af2b5675c4e74024c03f
Pedro Pinho <rihor.pedro@gmail.com>
2019-04-06 10:35:41 -0300

    made a lot of progress

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,61 @@
-const rangeCelEl = document.getElementById('range-cel');
-const rangeFahEl = document.getElementById('range-fah');
-const rangeKelEl = document.getElementById('range-kel');
+const [celSlide, fahSlide, kelSlide] = document.getElementsByClassName('div-temp-slider');
+const [celContainer, fahContainer, kelContainer] = document.getElementsByClassName('div-temp-bg');
+const [infoCel, infoFah, infoKel] = document.getElementsByClassName('temp-degrees');
 
-const tempCel = new Temperature(0);
-const tempFah = new Temperature(0);
-const tempKel = new Temperature(0);
+const body = document.querySelector('body');
 
+const celsius = new Temperature(celContainer, celSlide, infoCel);
+const fahrenheit = new Temperature(fahContainer, fahSlide, infoFah);
+const kelvin = new Temperature(kelContainer, kelSlide, infoKel);
 
-rangeCelEl.oninput = () => {
-    console.log(rangeCelEl.value);
+celsius.container.addEventListener('mousedown', startDrag, false);
+fahrenheit.container.addEventListener('mousedown', startDrag, false);
+kelvin.container.addEventListener('mousedown', startDrag, false);
+
+body.addEventListener('mouseup', stopSlide, false);
+
+function startDrag(event) {
+	event.preventDefault();
+	body.addEventListener('mousemove', moveSlide, false);
+	setSlidePosition(event, getContainer(event));
 }
 
-rangeFahEl.oninput = () => {
-    console.log(rangeFahEl.value);
+function moveSlide(event) {
+    event.preventDefault();
+	setSlidePosition(event, getContainer(event));
 }
 
-rangeKelEl.oninput = () => {
-    console.log(rangeKelEl.value);
+function stopSlide(event) {
+	event.preventDefault();	
+	getContainer(event).removeEventListener('mousemove', moveSlide, false);
+    setSlidePosition(event, getContainer(event));
+}
+
+function setSlidePosition(event, container) {
+	let position = getPosition(event, container);
+	let slider = getSlider(event);
+	slider.style.height = position + 'px';
+}
+
+function getContainer(event) {
+	if (event.target.children[0] == undefined) {		
+		return event.target.parentNode;
+	}
+	if (event.srcElement.tagName == 'LI'){
+		return event.srcElement.firstElementChild;
+	}
+	return event.target;
+}
+
+function getSlider(event) {
+	if (event.target.children[0] == undefined) {
+		return event.target;
+	} else {
+		return event.target.children[0];
+	}
+}
+
+// retorn a posição dentro do container
+function getPosition(event, container) {
+	return Math.abs(event.pageY - container.offsetTop - container.clientHeight);
 }
